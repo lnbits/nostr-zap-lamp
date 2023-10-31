760f884eb5de848be9a5dafa175b7c3487ae5627
dni ⚡ <office@dnilabs.com>
2023-10-31 07:30:09 +0100

    add readme to deploy

--- build-webinstaller.sh
+++ build-webinstaller.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 PROJECT_NAME=nostrZapLamp
 REPO=https://github.com/lnbits/nostr-zap-lamp/releases/download
-INSTALLER_PATH=./hardware-installer/public/firmware/esp32
+INSTALLER_PATH=./hardware-installer/public/firmware
 
 git clone https://github.com/lnbits/hardware-installer
 
+cp README.md ./hardware-installer/public/README.md
 cp versions.json ./hardware-installer/src/versions.json
 cp installer/config.js ./hardware-installer/src/config.js
-cp installer/Hero.jsx ./hardware-installer/src/Hero.jsx
 
 sed -i "s/%title%/$PROJECT_NAME/g" ./hardware-installer/index.html
 
 mkdir -p $INSTALLER_PATH
-for version in $(jq -r '.versions[]' ./hardware-installer/src/versions.json); do
-    mkdir -p $INSTALLER_PATH/$version
-    wget $REPO/$version/$PROJECT_NAME.ino.bin
-    mv $PROJECT_NAME.ino.bin $INSTALLER_PATH/$version
-    wget $REPO/$version/$PROJECT_NAME.ino.partitions.bin
-    mv $PROJECT_NAME.ino.partitions.bin $INSTALLER_PATH/$version
-    wget $REPO/$version/$PROJECT_NAME.ino.bootloader.bin
-    mv $PROJECT_NAME.ino.bootloader.bin $INSTALLER_PATH/$version
+for device in $(jq -r '.devices[]' ./hardware-installer/src/versions.json); do
+    for version in $(jq -r '.versions[]' ./hardware-installer/src/versions.json); do
+        mkdir -p $INSTALLER_PATH/$device/$version
+        wget $REPO/$version/$PROJECT_NAME.ino.bin
+        wget $REPO/$version/$PROJECT_NAME.ino.partitions.bin
+        wget $REPO/$version/$PROJECT_NAME.ino.bootloader.bin
+        mv $PROJECT_NAME* $INSTALLER_PATH/$device/$version
+    done
 done
